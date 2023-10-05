08c61d094cd73d3aee33f9633fa9c5f377b77c1a
asib ahmed <ahmedasib1@gmail.com>
2023-10-05 20:08:31 +0600

    array object access

--- my-app2/src/App.js
+++ my-app2/src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  const details = [{name:"Asib", mobile:"01753249719"},{name:"Goru", mobile:"0151111111"},{name:"Basur", mobile:"1234456788"}];
   return (
+    
     <div className="App">
       <h1>web world</h1>
-      <Person></Person>
-      <Person></Person>
-      <Person></Person>
-      <Person></Person>
+      {/* <Person name="Asib" mobile="01753249719"></Person>
+      <Person name="Goru" mobile="0151111111"></Person>
+      <Person name="Basur" mobile="1234456788"></Person> */}
+      {details.map(person => <Person name={person.name} mobile={person.mobile}></Person>)}
     </div>
   );
 }
 
-function Person() {
+function Person(props) {
   return (
     <div className = "person">
-      <h2>name: Asib</h2>
-      <p>phone 0123456789</p>
+      <h2>name: {props.name}</h2>
+      <p>phone: {props.mobile}</p>
     </div>
   )
 }
