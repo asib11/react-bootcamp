f15a01345014460b2318704b42e546ba0de0f76a
asib ahmed <ahmedasib1@gmail.com>
2023-10-05 22:16:25 +0600

    state work

--- my-app2/src/App.js
+++ my-app2/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {useState} from 'react';
 
 function App() {
   const details = [{name:"Asib", mobile:"01753249719"},{name:"Goru", mobile:"0151111111"},{name:"Basur", mobile:"1234456788"}];
@@ -12,6 +12,7 @@ function App() {
       <Person name="Goru" mobile="0151111111"></Person>
       <Person name="Basur" mobile="1234456788"></Person> */}
       {details.map(person => <Person name={person.name} mobile={person.mobile}></Person>)}
+      <Counter></Counter>
     </div>
   );
 }
@@ -21,6 +22,20 @@ function Person(props) {
     <div className = "person">
       <h2>name: {props.name}</h2>
       <p>phone: {props.mobile}</p>
+      
+    </div>
+  )
+}
+
+function Counter(){
+  const [count, setCount] = useState(0);
+  const increase = () => setCount(count+1);
+  const decrease = () => setCount(count-1);
+  return(
+    <div>
+      <h1>Count:{count}</h1>
+      <button onClick={increase}>increase</button>
+      <button onClick={decrease}>decrease</button>
     </div>
   )
 }
