fb5a2dd5d348b5f3eea36d0538b191ec82af03e5
asib <ahmedasib1@gmail.com>
2023-10-11 23:50:13 +0600

    fetch data from local json file

--- react-core/src/components/Dataloadapi/DataShow.jsx
+++ react-core/src/components/Dataloadapi/DataShow.jsx
@@ -1,11 +1,17 @@
-const DataShow = (props) => {
-    const {id, guid} = props.jData ;
+import PropTypes from 'prop-types'; 
+
+const DataShow = props => {
+    const {id,  balance} = props.jData ;
     return (
         <div>
             <h2>id: {id}</h2>
-            <h3>guid: {guid}</h3>
+            {/* <h3>guid: {guid}</h3> */}
+            <h4>price: {balance}</h4>
         </div>
     );
 };
 
+DataShow.propTypes = {
+  jData: PropTypes.object, // Use the appropriate PropTypes type
+};
 export default DataShow;
