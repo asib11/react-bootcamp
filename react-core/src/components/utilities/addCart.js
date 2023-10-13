0c1dd8100ec8e245f9b9703cecc842aceb3a9ea5
asib ahmed <ahmedasib1@gmail.com>
2023-10-13 11:32:17 +0600

    local storage add multiple item

--- react-core/src/components/utilities/addCart.js
+++ react-core/src/components/utilities/addCart.js
@@ -1,11 +1,32 @@
+// const fakeDB = id => {
+//     const quantity = localStorage.getItem(id);
+//     if(quantity){
+//         const newQuantity = parseInt(quantity) + 1;
+//         localStorage.setItem(id, newQuantity);
+//     }else{
+//         localStorage.setItem(id,1);
+//     }
+// }
+
 const fakeDB = id => {
-    const quantity = localStorage.getItem(id);
-    if(quantity){
-        const newQuantity = parseInt(quantity) + 1;
-        localStorage.setItem(id, newQuantity);
+    let shoppingCart;
+
+    const storedCart = localStorage.getItem('shopping-cart');
+    if(storedCart){
+        shoppingCart = JSON.parse(storedCart);
     }else{
-        localStorage.setItem(id,1);
+        shoppingCart = {};
     }
+
+    //add item
+    const quantity = shoppingCart[id];
+    if(quantity){
+        const newQuantity = quantity + 1;
+        shoppingCart[id] = newQuantity;
+    }else{
+        shoppingCart[id] = 1;
+    }
+    localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart));
 }
 
 export {fakeDB}
