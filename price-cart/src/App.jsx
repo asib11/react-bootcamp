feb657ff167facfad322747ae93d5c23f2eb82d9
asib <ahmedasib1@gmail.com>
2023-10-16 23:21:01 +0600

    react tailwind setup

---
+++ price-cart/src/App.jsx
@@ -0,0 +1,17 @@
+//import { useState } from 'react'
+import './App.css'
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className="text-7xl font-bold text-cyan-500 text-center">
+      Welcome to Price Cart
+    </h1>
+
+    </>
+  )
+}
+
+export default App
