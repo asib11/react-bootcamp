044d7a1382405595b45d8ce9b3796c713250dd00
asib <ahmedasib1@gmail.com>
2023-10-17 23:09:00 +0600

    react-tailwind responsive

--- price-cart/src/components/Link/Link.jsx
+++ price-cart/src/components/Link/Link.jsx
@@ -5,7 +5,6 @@ import PropTypes from 'prop-types';
 const Link = ({navBar}) => {
     return (
         <li className='md:flex mr-10'>
-            
             <a href={navBar.route}>{navBar.name}</a>
         </li>
     );
--- price-cart/src/components/Price/Price.jsx
+++ price-cart/src/components/Price/Price.jsx
@@ -10,12 +10,13 @@ const Price = () => {
         .then(data => setPrices(data))
     },[])
     return (
-        <div >
-            <h1 className='text-5xl bg-cyan-200 text-center mt-4 p-4'>affortable Price</h1>
-            <div className='grid grid-cols-3 gap-3'>
+        <div className='my-6 mx-4'>
+            <h1 className='text-5xl bg-cyan-200 text-center mt-4 px-2 py-4 uppercase font-extrabold rounded-3xl'>affortable Price</h1>
+            <div className='grid md:grid-cols-3 gap-12 '>
             {
                 prices.map(price =><PriceCart key ={price.id} price ={price}></PriceCart>)
             }
+            
             </div>
         </div>
     );
